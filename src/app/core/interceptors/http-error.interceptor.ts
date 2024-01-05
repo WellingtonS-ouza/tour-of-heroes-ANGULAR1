2be073ca2eaf4dd2c732f888f3dbdcd8648ca641
Wellington <wel.souza88@gmail.com>
2024-01-04 21:42:11 -0300

    Add token

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -8,6 +8,7 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -17,10 +18,20 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((err: HttpErrorResponse) => {
+
+        if (!environment.production) {
+          console.log(err)
+        }
+
+
         let errorMsg = ''
 
         if (err.error instanceof ErrorEvent) {
           errorMsg = `Error ${err.error.message}`
+        } else if (Array.isArray(err.error) && err.error.length) {
+          errorMsg = `Error: ${err.error[1]}`
+        } else if (err.error.error) {
+          errorMsg = `Error: ${err.error.error}`
         } else {
           errorMsg = `Error Code ${err.status}, Message: ${err.message}`
         }
